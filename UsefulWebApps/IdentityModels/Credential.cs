fc615877256a51b38df2c52e134f824e3726c084
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-08-23 13:44:50 -0400

    add data model classes for identity core login system

---
+++ UsefulWebApps/IdentityModels/Credential.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsefulWebApps.IdentityModels
+{
+    public class Credential
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ UsefulWebApps/IdentityModels/Register.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsefulWebApps.IdentityModels
+{
+    public class Register
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [DataType(dataType: DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
