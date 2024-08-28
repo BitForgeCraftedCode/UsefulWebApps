f9aab1ed1d5e3a97bd51f8ffd9e4900a60e34360
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-08-28 12:56:22 -0400

    add unit of work for recipe delete default get action method

--- UsefulWebApps/Controllers/MyRecipesController.cs
+++ UsefulWebApps/Controllers/MyRecipesController.cs
@@ -290,25 +290,9 @@ namespace UsefulWebApps.Controllers
                 return NotFound();
             }
 
-            //returns x rows of a single recipe at RecipeId where x is the number of categories
-            string sql = @"SELECT * FROM recipes
-                JOIN recipe_categories_join ON recipe_categories_join.RecipeId = recipes.RecipeId
-                JOIN recipe_categories ON recipe_categories_join.CategoryId = recipe_categories.CategoryId 
-                JOIN recipe_courses ON recipe_courses.CourseId = recipes.CourseId
-                JOIN recipe_cuisines ON recipe_cuisines.CuisineId = recipes.CuisineId
-                JOIN recipe_difficulties ON recipe_difficulties.DifficultyId = recipes.DifficultyId
-                WHERE recipes.RecipeId = @id;";
-
             //https://www.learndapper.com/relationships -- map the JOIN to C# objects
             //this is a list of 1 single recipe listed x times one for each category -- best to see this by running the above sql in workbench. 
-            List<Recipe> recipe = (List<Recipe>)await _connection.QueryAsync<Recipe, RecipeCategories, RecipeCourses, RecipeCuisines, RecipeDifficulties, Recipe>(sql,
-                (recipe, recipeCategories, recipeCourses, recipeCuisines, recipeDifficulties) => {
-                    recipe.Categories.Add(recipeCategories);
-                    recipe.Course = recipeCourses;
-                    recipe.Cuisine = recipeCuisines;
-                    recipe.Difficulty = recipeDifficulties;
-                    return recipe;
-                }, new { id }, splitOn: "CategoryId, CourseId, CuisineId, DifficultyId");
+            List<Recipe> recipe = await _unitOfWork.Recipe.GetRecipeById(id);
 
             //since we sql SELECT on 1 id GroupBy returns 1 group with x num recipe rows
             //foreach group get the First recipe and add the categories to it
@@ -321,7 +305,6 @@ namespace UsefulWebApps.Controllers
                 return singleRecipe;
             }).ToList();
 
-            await _connection.CloseAsync();
             return View(filteredRecipe[0]);
         }
 
