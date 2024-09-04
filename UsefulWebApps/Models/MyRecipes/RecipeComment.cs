3addf85b826ee9a865fbfeb1c0b4e016a256d050
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-09-04 11:07:20 -0400

    add recipe comment model

---
+++ UsefulWebApps/Models/MyRecipes/RecipeComment.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UsefulWebApps.Models.MyRecipes
+{
+    [Table("recipe_comments")]
+    public class RecipeComment
+    {
+        [Key]
+        [Column("CommentId")]
+        public int CommentId { get; set; }
+
+        [Column("Comment")]
+        [Required(ErrorMessage = "Recipe comment is required.")]
+        [StringLength(1000, MinimumLength = 10, ErrorMessage = "Please enter at least 10 characters.")]
+        public string Comment { get; set; } = string.Empty;
+
+        [Column("UserId")]
+        [Required(ErrorMessage = "User Id is required.")]
+        public string UserId { get; set; } = string.Empty;
+
+        [Column("UserName")]
+        [Required(ErrorMessage = "User Name is required.")]
+        public string UserName { get; set; } = string.Empty;
+
+        [ForeignKey("RecipeId")]
+        [Column("RecipeId")]
+        public int RecipeId { get; set; }
+    }
+}
