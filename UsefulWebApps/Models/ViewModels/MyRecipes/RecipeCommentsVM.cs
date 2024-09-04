1b071c9e45539a78efe06d5903d463106e487e6c
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-09-04 11:29:36 -0400

    add recipe comments VM class

---
+++ UsefulWebApps/Models/ViewModels/MyRecipes/RecipeCommentsVM.cs
@@ -0,0 +1,11 @@
+﻿using UsefulWebApps.Models.MyRecipes;
+
+namespace UsefulWebApps.Models.ViewModels.MyRecipes
+{
+    public class RecipeCommentsVM
+    {
+        public Recipe Recipe { get; set; }
+
+        public List<RecipeComment> RecipeComments { get; set; }
+    }
+}
