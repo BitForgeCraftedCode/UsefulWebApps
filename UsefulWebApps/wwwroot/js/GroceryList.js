0cf4577694fa13455232715f44fd3a4274edbcdd
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-09-23 10:41:02 -0400

    add JS file for grocery list view

---
+++ UsefulWebApps/wwwroot/js/GroceryList.js
@@ -0,0 +1,29 @@
+﻿$("#delete-all-grocery-list").validate({
+    submitHandler: function (form) {
+        //ask to confirm delete
+        toastConfirm().then(deleteConfirm => {
+            if (deleteConfirm == true) {
+                form.submit();
+            }
+            else {
+                toastr.success("Item has NOT been deleted");
+            }
+        });
+    }
+});
+//have to loop through each form and submit "this" one that is clicked
+$(".grocery-list-delete-item").each(function () {
+    $(this).validate({
+        submitHandler: function (form) {
+            //ask to confirm delete
+            toastConfirm().then(deleteConfirm => {
+                if (deleteConfirm == true) {
+                    form.submit();
+                }
+                else {
+                    toastr.success("Item has NOT been deleted");
+                }
+            });
+        }
+    });
+});
