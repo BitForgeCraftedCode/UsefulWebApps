84b0d328257ed74b74bb2a511b2f6980f7da2503
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-10-25 12:22:00 -0400

    fix case sensitive MySql issue on production server add linux V8 Engine for sass

--- UsefulWebApps/Data/EFCoreDbContext.cs
+++ UsefulWebApps/Data/EFCoreDbContext.cs
@@ -6,5 +6,19 @@ namespace UsefulWebApps.Data
     public class EFCoreDbContext : IdentityDbContext
     {
         public EFCoreDbContext(DbContextOptions<EFCoreDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            foreach (var entity in builder.Model.GetEntityTypes())
+            {
+                //set table names to lower case
+                //the problem was development PC runs Windows, and the server runs Linux, Linux having a case-sensitive MySQL installation
+                //the table names for Identity Core in the MySql database are all lower case but EF Core setting them as CamelCase
+                //So just set them as lower. 
+                //wasnt a problem in Windows Dev as that is NOT case sensitive but production linux server is case sensitive
+                entity.SetTableName(entity.GetTableName().ToLower());
+            }
+        }
     }
 }
