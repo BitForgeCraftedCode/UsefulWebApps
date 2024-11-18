f3a31a7bd34305da78f6116cdc298dd3fb9ba244
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-11-18 15:37:34 -0500

    add basic artwork view controller and images

---
+++ UsefulWebApps/Controllers/ArtWorkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsefulWebApps.Controllers
+{
+    public class ArtWorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
