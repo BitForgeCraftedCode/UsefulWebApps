ad00e86d459e9c952f820a8d758e00f91054fe04
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-11-19 11:33:39 -0500

    add pagination for all artwork images in folder

--- UsefulWebApps/Controllers/ArtWorkController.cs
+++ UsefulWebApps/Controllers/ArtWorkController.cs
@@ -1,12 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using UsefulWebApps.Models.ViewModels.ArtWork;
 
 namespace UsefulWebApps.Controllers
 {
     public class ArtWorkController : Controller
     {
-        public IActionResult Index()
+        private IWebHostEnvironment Environment;
+        public ArtWorkController(IWebHostEnvironment _environment)
         {
-            return View();
+            Environment = _environment;
+        }
+        public IActionResult Index(int page)
+        {
+            IEnumerable<string> paths = Directory.EnumerateFiles(Path.Combine(this.Environment.WebRootPath, "images/artwork/"));
+            List<string> files = new List<string>();
+            List<string> filesToShow = new List<string>();
+            if (page == 0)
+            {
+                page = 1;
+            }
+           
+            int limit = 4;
+            int offset = (limit * (page - 1));
+            //count is total number of images
+            int count = paths.Count();
+            int totalPages = (int)Math.Ceiling(count / (double)limit);
+
+            foreach (string path in paths)
+            {
+                files.Add(Path.GetFileName(path));
+            }
+
+            if (page < totalPages)
+            {
+                //works until last page because last page may have less than 10 elements
+                filesToShow = files.GetRange(offset, limit);
+            }
+            //for last page skip the rest
+            else if (page == totalPages) 
+            {
+                filesToShow = new List<string>(files.Skip(offset));
+        
+            }
+            
+            ArtWorkVM artWorkVM = new() 
+            { 
+                FilesToShow = filesToShow,
+                CurrentPage = page,
+                TotalPages = totalPages,
+            };
+            return View(artWorkVM);
         }
     }
 }
---
+++ UsefulWebApps/Models/ViewModels/ArtWork/ArtWorkVM.cs
@@ -0,0 +1,9 @@
+﻿namespace UsefulWebApps.Models.ViewModels.ArtWork
+{
+    public class ArtWorkVM
+    {
+        public List<string> FilesToShow { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
