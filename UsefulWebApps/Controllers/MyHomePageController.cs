d61fe024115bb3052a823af5d49f11a1d15430a3
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-11-21 15:45:19 -0500

    add basic files and code for Custom Home page feature

---
+++ UsefulWebApps/Controllers/MyHomePageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsefulWebApps.Controllers
+{
+    public class MyHomePageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
