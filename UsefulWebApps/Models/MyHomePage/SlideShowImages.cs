7d8dd5f1a14ba52f6932531349cfcdbb545f5ed9
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-12-09 15:18:50 -0500

    add model classes for slide show

---
+++ UsefulWebApps/Models/MyHomePage/SlideShowImages.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UsefulWebApps.Models.MyHomePage
+{
+    [Table("slideshow_images")]
+    public class SlideShowImages
+    {
+        [Key]
+        [Column("SlideShowImageId")]
+        public int SlideShowImageId { get; set; }
+
+        [Column("ImagePath")]
+        public string ImagePath { get; set; } = string.Empty;
+
+        [Column("FolderName")]
+        public string FolderName { get; set; } = string.Empty;
+
+    }
+}
---
+++ UsefulWebApps/Models/MyHomePage/UserSlideShowImages.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UsefulWebApps.Models.MyHomePage
+{
+    [Table("user_slideshow_images")]
+    public class UserSlideShowImages
+    {
+        [Key]
+        [Column("UserSlideShowImageId")]
+        public int UserSlideShowImageId { get; set; }
+
+        [Column("UserId")]
+        public string UserId { get; set; } = string.Empty;
+
+        [Column("UserName")]
+        public string UserName { get; set; } = string.Empty;
+
+        [ForeignKey("SlideShowImageId")]
+        [Column("SlideShowImageId")]
+        public int SlideShowImageId { get; set; }
+    }
+}
