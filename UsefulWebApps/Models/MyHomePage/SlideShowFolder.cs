0ab20ab7f30e8c93d6c910aeab6efb46b6625bfe
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-12-10 11:27:05 -0500

    add model classes for select slideshow page

---
+++ UsefulWebApps/Models/MyHomePage/SlideShowFolder.cs
@@ -0,0 +1,8 @@
+﻿namespace UsefulWebApps.Models.MyHomePage
+{
+    public class SlideShowFolder
+    {
+        public string FolderName { get; set; } = string.Empty;
+        public bool IsSelected { get; set; }
+    }
+}
---
+++ UsefulWebApps/Models/ViewModels/MyHomePage/SelectSlideShowVM.cs
@@ -0,0 +1,9 @@
+﻿using UsefulWebApps.Models.MyHomePage;
+
+namespace UsefulWebApps.Models.ViewModels.MyHomePage
+{
+    public class SelectSlideShowVM
+    {
+        public List<SlideShowFolder> SlideShowFolders { get; set; }
+    }
+}
