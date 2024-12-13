9bb4373a6dedf4c684757f089b88299924ac89e8
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-12-13 11:45:01 -0500

    alter to do list sql table

--- ToDoList.sql
+++ ToDoList.sql
@@ -27,3 +27,17 @@ SELECT * FROM to_do_list WHERE UserId = '818fd1e7-05ab-44f5-9276-68f20ec3c70d';
 DELETE FROM usefulwebapps.to_do_list WHERE ID>=1;
 
 ALTER TABLE usefulwebapps.to_do_list AUTO_INCREMENT = 1;
+
+/*adding multiple to do lists for users*/
+/*1st delete all current to do lists -- sorry*/
+DELETE FROM to_do_list;
+/*2nd add a title col -- each list will have a title (only way i can think to do this. Not perfect but will do)*/
+ALTER TABLE to_do_list ADD ListTitle varchar(100) NOT NULL;
+
+INSERT INTO to_do_list (ToDoItem, Complete, UserId, ListTitle) VALUES ("Buy Beer", False, "251d80ae-93a3-401c-9be9-1ef83e30d541", "Shopping");
+INSERT INTO to_do_list (ToDoItem, Complete, UserId, ListTitle) VALUES ("Buy whiskey", False, "251d80ae-93a3-401c-9be9-1ef83e30d541", "Shopping");
+INSERT INTO to_do_list (ToDoItem, Complete, UserId, ListTitle) VALUES ("gift for work party", False, "251d80ae-93a3-401c-9be9-1ef83e30d541", "Shopping");
+
+INSERT INTO to_do_list (ToDoItem, Complete, UserId, ListTitle) VALUES ("change generator oil", False, "251d80ae-93a3-401c-9be9-1ef83e30d541", "House Work");
+INSERT INTO to_do_list (ToDoItem, Complete, UserId, ListTitle) VALUES ("paint bathroom", False, "251d80ae-93a3-401c-9be9-1ef83e30d541", "House Work");
+INSERT INTO to_do_list (ToDoItem, Complete, UserId, ListTitle) VALUES ("build shoe rack", False, "251d80ae-93a3-401c-9be9-1ef83e30d541", "House Work");
