58cd2582063a511a8ecda0f0a7f0f940682308a1
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-12-30 11:44:55 -0500

    add basic weather view

---
+++ UsefulWebApps/Controllers/WeatherController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsefulWebApps.Controllers
+{
+    [Authorize(Roles = "StandardUser, Admin")]
+    [AutoValidateAntiforgeryToken]
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
