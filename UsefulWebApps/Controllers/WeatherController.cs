ae40ae58952dd845ee064426241c6475daac387a
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2024-12-30 13:36:13 -0500

    add basic method to get hidden api key from XML doc dont commit doc to git

--- UsefulWebApps/Controllers/WeatherController.cs
+++ UsefulWebApps/Controllers/WeatherController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using UsefulWebApps.Helpers.Weather;
 
 namespace UsefulWebApps.Controllers
 {
@@ -7,9 +8,23 @@ namespace UsefulWebApps.Controllers
     [AutoValidateAntiforgeryToken]
     public class WeatherController : Controller
     {
+        private static string apiKey = ManageAPIKey.GetAPIKey();
+        private static string json = string.Empty;
+        private static readonly HttpClient client = new HttpClient();
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> GetWeather()
+        {
+
+            string city = "Denville";
+            string state = "NJ";
+            string country = "US";
+            json = await client.GetStringAsync($"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit=1&appid={apiKey}");
+            Console.WriteLine(json);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ UsefulWebApps/Helpers/Weather/ManageAPIKey.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+
+namespace UsefulWebApps.Helpers.Weather
+{
+    public static class ManageAPIKey
+    {
+        private static string appDirectory = Directory.GetCurrentDirectory();
+        public static string GetAPIKey()
+        {
+            XDocument xmlDoc = new XDocument();
+            string docApiKey = string.Empty;
+            try
+            {
+                xmlDoc = XDocument.Load($"{appDirectory}/Helpers/Weather/APIKEY.xml");
+                docApiKey = xmlDoc.Element("ApiKey").Value;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to get api key from xml");
+                Console.WriteLine(e);
+            }
+            return docApiKey;
+        }
+    }
+}
