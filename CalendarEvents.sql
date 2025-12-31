28a0d1a2bf010ff674b1bfe9cadeb9c9a1f93243
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2025-12-30 20:00:00 -0400

    add calendar holiday sql

--- CalendarEvents.sql
+++ CalendarEvents.sql
@@ -20,3 +20,158 @@ CREATE TABLE calendar_events (
     INDEX idx_user_start (UserId, StartDate),
     PRIMARY KEY (`Id`)
 );
+
+/*Federal Holidays RRule's inserted via web server command line*/
+
+/*New Year's Day*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'New Year''s Day',
+  '2025-01-01 00:00:00',
+  '2025-01-01 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=1;BYMONTHDAY=1'
+);
+
+/*Martin Luther King Jr. Day*/
+/*3rd Monday in January*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Martin Luther King Jr. Day',
+  '2025-01-20 00:00:00',
+  '2025-01-20 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=1;BYDAY=MO;BYSETPOS=3'
+);
+
+/*Washington's Birthday*/
+/*3rd Monday in February*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Washington''s Birthday',
+  '2025-02-17 00:00:00',
+  '2025-02-17 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=2;BYDAY=MO;BYSETPOS=3'
+);
+
+/*Memorial Day*/
+/*Last Monday in May*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Memorial Day',
+  '2025-05-26 00:00:00',
+  '2025-05-26 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=5;BYDAY=MO;BYSETPOS=-1'
+);
+
+/*Juneteenth*/
+/*June 19*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Juneteenth National Independence Day',
+  '2025-06-19 00:00:00',
+  '2025-06-19 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=19'
+);
+
+/*Independence Day*/
+/*July 4*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Independence Day',
+  '2025-07-04 00:00:00',
+  '2025-07-04 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=7;BYMONTHDAY=4'
+);
+
+/*Labor Day*/
+/*1st Monday in September*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Labor Day',
+  '2025-09-01 00:00:00',
+  '2025-09-01 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=9;BYDAY=MO;BYSETPOS=1'
+);
+
+/*Columbus Day*/
+/*2nd Monday in October*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Columbus Day',
+  '2025-10-13 00:00:00',
+  '2025-10-13 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=10;BYDAY=MO;BYSETPOS=2'
+);
+
+/*Veterans Day*/
+/*November 11*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Veterans Day',
+  '2025-11-11 00:00:00',
+  '2025-11-11 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=11;BYMONTHDAY=11'
+);
+
+/*Thanksgiving Day*/
+/*4th Thursday in Novembe*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Thanksgiving Day',
+  '2025-11-27 00:00:00',
+  '2025-11-27 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=11;BYDAY=TH;BYSETPOS=4'
+);
+
+/*Christmas Day*/
+/*December 25*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Christmas Day',
+  '2025-12-25 00:00:00',
+  '2025-12-25 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=25'
+);
