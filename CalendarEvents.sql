fd0c738080feefc61a565f5eca7052e2432250ce
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2026-02-18 20:00:00 -0400

    add new holidays to database

--- CalendarEvents.sql
+++ CalendarEvents.sql
@@ -149,7 +149,7 @@ VALUES
 );
 
 /*Thanksgiving Day*/
-/*4th Thursday in Novembe*/
+/*4th Thursday in November*/
 INSERT INTO calendar_events
 (UserId, Title, StartDate, EndDate, IsAllDay, RRule)
 VALUES
@@ -160,6 +160,19 @@ VALUES
   '2025-11-27 23:59:59',
   1,
   'FREQ=YEARLY;BYMONTH=11;BYDAY=TH;BYSETPOS=4'
+);
+
+/*Christmas Eve*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Christmas Eve',
+  '2025-12-24 00:00:00',
+  '2025-12-24 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=24'
 );
 
 /*Christmas Day*/
@@ -174,4 +187,162 @@ VALUES
   '2025-12-25 23:59:59',
   1,
   'FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=25'
+);
+
+/*New Year's Eve*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'New Year''s Eve',
+  '2025-12-31 00:00:00',
+  '2025-12-31 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=31'
+);
+
+/*Pearl Harbor Remembrance Day*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Pearl Harbor Remembrance Day',
+  '2025-12-07 00:00:00',
+  '2025-12-07 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=12;BYMONTHDAY=7'
+);
+
+/*Patriot Day (September 11)*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Patriot Day',
+  '2025-09-11 00:00:00',
+  '2025-09-11 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=9;BYMONTHDAY=11'
+);
+
+/*Valentine's Day*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Valentine''s Day',
+  '2025-02-14 00:00:00',
+  '2025-02-14 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=2;BYMONTHDAY=14'
+);
+
+/*St. Patrick's Day*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'St. Patrick''s Day',
+  '2025-03-17 00:00:00',
+  '2025-03-17 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=3;BYMONTHDAY=17'
+);
+
+/*Mother's Day*/
+/*2nd Sunday in May*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Mother''s Day',
+  '2025-05-11 00:00:00',
+  '2025-05-11 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=5;BYDAY=SU;BYSETPOS=2'
+);
+
+/*Father's Day*/
+/*3rd Sunday in June*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Father''s Day',
+  '2025-06-15 00:00:00',
+  '2025-06-15 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=6;BYDAY=SU;BYSETPOS=3'
+);
+
+/*Halloween*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Halloween',
+  '2025-10-31 00:00:00',
+  '2025-10-31 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=10;BYMONTHDAY=31'
+);
+
+/*Cinco de Mayo*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Cinco de Mayo',
+  '2025-05-05 00:00:00',
+  '2025-05-05 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=5;BYMONTHDAY=5'
+);
+
+/*April Fool''s Day*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'April Fool''s Day',
+  '2025-04-01 00:00:00',
+  '2025-04-01 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=4;BYMONTHDAY=1'
+);
+
+/*Earth Day*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Earth Day',
+  '2025-04-22 00:00:00',
+  '2025-04-22 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=4;BYMONTHDAY=22'
+);
+
+/*Flag Day*/
+INSERT INTO calendar_events
+(UserId, Title, StartDate, EndDate, IsAllDay, RRule)
+VALUES
+(
+  NULL,
+  'Flag Day',
+  '2025-06-14 00:00:00',
+  '2025-06-14 23:59:59',
+  1,
+  'FREQ=YEARLY;BYMONTH=6;BYMONTHDAY=14'
 );
