dee58644769156d9bbe43b0e58b52cf159e1388e
BitForgeCraftedCode <11381086+BitForgeCraftedCode@users.noreply.github.com>
2026-05-26 20:00:00 -0400

    add a view to see your notifications

---
+++ UsefulWebApps/Controllers/NotificationsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UsefulWebApps.Helpers;
+using UsefulWebApps.Models.Notifications;
+using UsefulWebApps.Repository.IRepository;
+
+namespace UsefulWebApps.Controllers
+{
+    [Authorize(Roles = "StandardUser, Admin")]
+    [AutoValidateAntiforgeryToken]
+    public class NotificationsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public NotificationsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IActionResult> Index()
+        {
+            string? userId = User.GetUserId();
+            if (string.IsNullOrEmpty(userId)) return NotFound();
+
+            List<Notifications> notifications = (await _unitOfWork.Notifications.GetAllWhere("UserId", userId))
+                .OrderByDescending(n => n.CreatedAt)
+                .ToList();
+            return View(notifications);
+        }
+    }
+}
